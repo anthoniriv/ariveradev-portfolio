e1b8570bcaa1ba7e401b3a2370a82048be348c1a
Anthoni Rivera <anthoniriv01@gmail.com>
2023-04-08 19:47:04 -0500

    Finish

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,14 +4,14 @@ import { AiFillGithub, AiFillLinkedin, AiFillYoutube } from "react-icons/ai";
 import Image from "next/image";
 import deved from "../../public/dev-ed-wave.png";
 import design from "../../public/design.png";
-import consulting from "../../public/consulting.png";
+import camera from "../../public/camera.png";
 import code from "../../public/code.png";
-import web1 from "../../public/web1.png";
-import web2 from "../../public/web2.png";
-import web3 from "../../public/web3.png";
-import web4 from "../../public/web4.png";
-import web5 from "../../public/web5.png";
-import web6 from "../../public/web6.png";
+import creativehome from "../../public/creativehome.png";
+import lomamarilla from "../../public/lomamarilla.png";
+import santed from "../../public/santed.png";
+import dsegunda from "../../public/dsegunda.png";
+import hoobank from "../../public/hoobank.png";
+import impuestosperu from "../../public/impuestosperu.png";
 import { useState } from "react";
 
 export default function Home() {
@@ -28,7 +28,7 @@ export default function Home() {
         <section className="min-h-screen">
           <nav className="py-10 mb-12 flex justify-between">
             <h1 className="ease-in-out duration-300 text-xl font-burtons dark:text-white">
-              developedbysd
+              anthoniriv
             </h1>
             <ul className="flex items-center">
               <li>
@@ -39,7 +39,9 @@ export default function Home() {
               </li>
               <li>
                 <a
-                  href="#"
+                  href="https://mega.nz/file/yLJQESpC#cR50hZrgDRM3iDQWaef1XTB6F-b7do3rzZURJtC0DEw"
+                  target="_blank"
+                  rel="noopener noreferrer"
                   className="bg-gradient-to-r from-cyan-500 to-blue-500 text-white px-4 py-2 rounded-md ml-8"
                 >
                   Resume
@@ -59,9 +61,27 @@ export default function Home() {
             </p>
           </div>
           <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600 dark:text-white">
-            <AiFillGithub className="cursor-pointer hover:text-blue-600 ease-in-out duration-300" />
-            <AiFillLinkedin className="cursor-pointer hover:text-blue-600 ease-in-out duration-300" />
-            <AiFillYoutube className="cursor-pointer hover:text-blue-600 ease-in-out duration-300" />
+            <a
+              href="https://github.com/anthoniriv"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <AiFillGithub className="cursor-pointer hover:text-blue-600 ease-in-out duration-300" />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/anthoniriv01"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <AiFillLinkedin className="cursor-pointer hover:text-blue-600 ease-in-out duration-300" />
+            </a>
+            <a
+              href="https://www.youtube.com/@anthoniriv"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <AiFillYoutube className="cursor-pointer hover:text-blue-600 ease-in-out duration-300" />
+            </a>
           </div>
           <div className="ease-in-out duration-300 relative mx-auto bg-gradient-to-b from-blue-500 rounded-full w-80 h-80 mt-20 overflow-hidden md:h-96 md:w-96">
             <Image src={deved} layout="fill" objectFit="cover" />
@@ -74,17 +94,18 @@ export default function Home() {
               Servicios que ofrezco
             </h3>
             <p className="text-md py-2 leading-8 text-gray-800 dark:text-white">
-              Desde el comienzo de mi carrera como diseñador y desarrollador
-              independiente, he trabajado de forma remota para
-              <span className="text-blue-500"> agencias </span>
-              he consultado para{" "}
-              <span className="text-blue-500">startups </span>y he colaborado
-              con personas talentosas para crear productos digitales tanto para
-              uso empresarial como de consumo.
-            </p>
-            <p className="text-md py-2 leading-8 text-gray-800 dark:text-white">
-              Ofrezco una amplia gama de servicios, que incluyen diseño de
-              marca, programación y enseñanza.
+              Ofrezco servicios de diseño y desarrollo de páginas web de alta
+              calidad y edición audiovisual para clientes en diferentes
+              sectores. Mi enfoque se centra en crear{" "}
+              <span className="text-blue-500"> soluciones personalizadas </span>
+              y eficientes que satisfagan las necesidades específicas de cada
+              proyecto. Como experto en diseño web, utilizo las últimas
+              tendencias y tecnologías para crear sitios{" "}
+              <span className="text-blue-500"> web modernos</span>, atractivos y
+              funcionales. Además, también ofrezco servicios de{" "}
+              <span className="text-blue-500">edición audiovisual </span>,
+              incluyendo la creación de vídeos y la postproducción para mejorar
+              la calidad y la estética de tus contenidos.
             </p>
           </div>
           <div className="lg:flex gap-10">
@@ -96,30 +117,25 @@ export default function Home() {
                 className="mx-auto"
               />
               <h3 className="text-lg font-medium pt-8 pb-2 dark:text-white">
-                Diseños hermosos
+                Diseños modernos
               </h3>
               <p className="py-2 md:text-xl dark:text-white">
                 Creación de diseños elegantes adaptados a sus necesidades
-                siguiendo la teoría del diseño.
+                siguiendo las tendencias del día a día.
               </p>
               <h4 className="py-4 text-blue-600 dark:text-white">
                 Herramientas de diseño que utilizo
               </h4>
               <p className="text-gray-800 py-1 dark:text-white">Photoshop</p>
-              <p className="text-gray-800 py-1 dark:text-white">Illustrator</p>
+              <p className="text-gray-800 py-1 dark:text-white">AdobeXD</p>
               <p className="text-gray-800 py-1 dark:text-white">Figma</p>
             </div>
             <div className="ease-in-out duration-300 text-center shadow-lg p-10 rounded-xl my-10 dark:bg-gradient-to-b from-blue-500">
-              <Image
-                src={consulting}
-                width={100}
-                height={100}
-                className="mx-auto"
-              />
+              <Image src={code} width={100} height={100} className="mx-auto" />
               <h3 className="text-lg font-medium pt-8 pb-2 dark:text-white">
                 Sitios web modernos
               </h3>
-              <p className="py-2 dark:text-white">
+              <p className="py-2 md:text-xl dark:text-white">
                 Creación de diseños elegantes adaptados a sus necesidades
                 siguiendo la teoría del diseño.
               </p>
@@ -131,20 +147,29 @@ export default function Home() {
               <p className="text-gray-800 py-1 dark:text-white">Next.js</p>
             </div>
             <div className="text-center shadow-lg p-10 rounded-xl my-10 dark:bg-gradient-to-b from-blue-500">
-              <Image src={code} width={100} height={100} className="mx-auto" />
+              <Image
+                src={camera}
+                width={100}
+                height={100}
+                className="mx-auto"
+              />
               <h3 className="text-lg font-medium pt-8 pb-2 dark:text-white">
-                Modern Websites
+                Videos atractivos
               </h3>
-              <p className="py-2 dark:text-white">
-                Creating elegant designs suited for your needs following core
-                design theory.
+              <p className="py-2 md:text-xl dark:text-white">
+                Creación de diseños elegantes adaptados a sus necesidades
+                siguiendo la teoría del diseño.
               </p>
               <h4 className="py-4 text-blue-600 dark:text-white">
-                Designing tools I use
+                Herramientas de edicion que utilizo
               </h4>
-              <p className="text-gray-800 py-1 dark:text-white">Photoshop</p>
-              <p className="text-gray-800 py-1 dark:text-white">Illustrator</p>
-              <p className="text-gray-800 py-1 dark:text-white">Figma</p>
+              <p className="text-gray-800 py-1 dark:text-white">
+                Davinci Resolve
+              </p>
+              <p className="text-gray-800 py-1 dark:text-white">Premiere</p>
+              <p className="text-gray-800 py-1 dark:text-white">
+                After Effects
+              </p>
             </div>
           </div>
         </section>
@@ -152,72 +177,107 @@ export default function Home() {
           <div>
             <h3 className="text-3xl py-1 dark:text-white">Portafolio</h3>
             <p className="text-md py-2 leading-8 text-gray-800 dark:text-white">
-              Desde el comienzo de mi carrera como diseñador y desarrollador
-              freelance, he trabajado de forma remota para
-              <span className="text-blue-500"> agencias </span>
-              consultado para<span className="text-blue-500 ">startups </span>y
-              colaborado con personas talentosas para crear productos digitales
-              tanto para uso empresarial como para consumidores.
-            </p>
-            <p className="text-md py-2 leading-8 text-gray-800 dark:text-white">
-              Ofrezco una amplia gama de servicios, incluyendo diseño de marca,
-              programación y enseñanza.
+              Soy un desarrollador Full Stack con
+              <span className="text-blue-500"> amplia experiencia </span> en la
+              creación de soluciones personalizadas y eficientes para una
+              variedad de clientes en diferentes industrias. Mi portafolio
+              incluye una amplia variedad de{" "}
+              <span className="text-blue-500">proyectos exitosos </span>, desde
+              aplicaciones móviles hasta landing pages y aplicaciones web.
+              Siempre estoy en busca de nuevos desafíos y soluciones innovadoras
+              para satisfacer las necesidades de mis clientes.
             </p>
           </div>
           <div className="flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap">
             <div className="basis-1/3 flex-1">
-              <Image
-                src={web1}
-                className="rounded-lg object-cover"
-                width={"100%"}
-                height={"100%"}
-                layout="responsive"
-              />
+              <a
+                href="https://creativehome.pe/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <Image
+                  src={creativehome}
+                  className="rounded-lg object-cover"
+                  width={"100%"}
+                  height={"100%"}
+                  layout="responsive"
+                />
+              </a>
             </div>
             <div className="basis-1/3 flex-1">
-              <Image
-                src={web2}
-                className="rounded-lg object-cover"
-                width={"100%"}
-                height={"100%"}
-                layout="responsive"
-              />
+              <a
+                href="https://edificiolomaamarilla.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <Image
+                  src={lomamarilla}
+                  className="rounded-lg object-cover"
+                  width={"100%"}
+                  height={"100%"}
+                  layout="responsive"
+                />
+              </a>
             </div>
             <div className="basis-1/3 flex-1">
-              <Image
-                src={web3}
-                className="rounded-lg object-cover"
-                width={"100%"}
-                height={"100%"}
-                layout="responsive"
-              />
+              <a
+                href="https://www.santed.com.pe/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <Image
+                  src={santed}
+                  className="rounded-lg object-cover"
+                  width={"100%"}
+                  height={"100%"}
+                  layout="responsive"
+                />
+              </a>
             </div>
             <div className="basis-1/3 flex-1">
-              <Image
-                src={web4}
-                className="rounded-lg object-cover"
-                width={"100%"}
-                height={"100%"}
-                layout="responsive"
-              />
+              <a
+                href="https://dsegunda.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <Image
+                  src={dsegunda}
+                  className="rounded-lg object-cover"
+                  width={"100%"}
+                  height={"100%"}
+                  layout="responsive"
+                />
+              </a>
             </div>
             <div className="basis-1/3 flex-1">
-              <Image
-                src={web5}
-                className="rounded-lg object-cover"
-                width={"100%"}
-                height={"100%"}
-                layout="responsive"
-              />
+              <a
+                href="https://banco-webmodern.vercel.app/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <Image
+                  src={hoobank}
+                  className="rounded-lg object-cover"
+                  width={"100%"}
+                  height={"100%"}
+                  layout="responsive"
+                />
+              </a>
             </div>
             <div className="basis-1/3 flex-1">
-              <Image
-                src={web6}
-                className="rounded-lg object-cover"
-                width={"100%"}
-                height={"100%"}
-                layout="responsive"
-              />
+              <a
+                href="https://play.google.com/store/apps/details?id=com.impuestosperu&hl=es_PE"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <Image
+                  src={impuestosperu}
+                  className="rounded-lg object-cover"
+                  width={"100%"}
+                  height={"100%"}
+                  layout="responsive"
+                />
+              </a>
             </div>
           </div>
         </section>
